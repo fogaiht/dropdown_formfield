a9c4e62096e58a7db9abb8c4a8763b6e27124883
Thiago Fernandes <thiagofpf@gmail.com>
2020-09-23 17:09:32 -0300

    added some colors and funtions

--- lib/dropdown_formfield.dart
+++ lib/dropdown_formfield.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class DropDownFormField extends FormField<dynamic> {
   final String titleText;
   final String hintText;
+  final TextStyle itemsTextStyle;
   final bool required;
   final String errorText;
   final dynamic value;
@@ -12,10 +13,14 @@ class DropDownFormField extends FormField<dynamic> {
   final String textField;
   final String valueField;
   final Function onChanged;
+  final Function onTap;
   final bool filled;
+  final bool isDense;
   final EdgeInsets contentPadding;
   final Color hintColorText;
-  final TextStyle itemsTextStyle;
+  final Color dropdownColor;
+  final Color iconDisabledColor;
+  final Color iconEnabledColor;
 
   DropDownFormField({
     FormFieldSetter<dynamic> onSaved,
@@ -24,6 +29,9 @@ class DropDownFormField extends FormField<dynamic> {
     this.titleText = 'Title',
     this.hintText = 'Select one option',
     this.hintColorText,
+    this.dropdownColor,
+    this.iconDisabledColor,
+    this.iconEnabledColor,
     this.itemsTextStyle,
     this.required = false,
     this.errorText = 'Please select one option',
@@ -32,7 +40,9 @@ class DropDownFormField extends FormField<dynamic> {
     this.textField,
     this.valueField,
     this.onChanged,
+    this.onTap,
     this.filled = true,
+    this.isDense = false,
     this.contentPadding = const EdgeInsets.fromLTRB(12, 12, 8, 0),
   }) : super(
           onSaved: onSaved,
@@ -52,6 +62,11 @@ class DropDownFormField extends FormField<dynamic> {
                     ),
                     child: DropdownButtonHideUnderline(
                       child: DropdownButton<dynamic>(
+                        dropdownColor: dropdownColor,
+                        iconDisabledColor: iconDisabledColor,
+                        iconEnabledColor: iconEnabledColor,
+                        isDense: isDense,
+                        onTap: onTap,
                         isExpanded: true,
                         hint: Text(
                           hintText,
